5bc27784ac54ba814f76d7cc493f6eb1acca76a7
win811 <cuongwin811@gmail.com>
2020-09-23 16:51:09 +0700

    lan dau

---
+++ src/main/java/com/c0320g1/vaccine/VaccineApplication.java
@@ -0,0 +1,13 @@
+package com.c0320g1.vaccine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaccineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/c0320g1/vaccine/VaccineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c0320g1.vaccine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
