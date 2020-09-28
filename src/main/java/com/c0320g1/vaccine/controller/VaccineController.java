0debdb0e818ead2d92e9560205cb5263fa1ff81b
win811 <cuongwin811@gmail.com>
2020-09-28 10:31:10 +0700

    B-Cuong update VaccineController

---
+++ src/main/java/com/c0320g1/vaccine/controller/VaccineController.java
@@ -0,0 +1,45 @@
+package com.c0320g1.vaccine.controller;
+import com.c0320g1.vaccine.model.Vaccine;
+import com.c0320g1.vaccine.service.VaccineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:4200",allowedHeaders = "*")
+@RestController
+@RequestMapping("/api/v1")
+public class VaccineController {
+
+    @Autowired
+    private VaccineService vaccineService;
+
+    @GetMapping("/vaccine-storage")
+    public ResponseEntity<Page<Vaccine>> getVaccineStorage(@RequestParam(defaultValue = "") String name,
+                                                           @RequestParam(defaultValue = "") String category,
+                                                           @RequestParam(defaultValue = "") String country,
+                                                           @RequestParam(defaultValue = "") String inventoryStatus,
+                                                           @RequestParam(defaultValue = "0") int page
+                                                           ) {
+        Page<Vaccine> vaccinePage = this.vaccineService.findBySpec(name,category,country,inventoryStatus,page);
+        return ResponseEntity.ok(vaccinePage);
+    }
+
+    @PostMapping("/import-vaccine")
+    public ResponseEntity<Vaccine> importVaccine(@RequestBody Vaccine vaccine) {
+        return ResponseEntity.ok(vaccineService.save(vaccine));
+    }
+
+    @PutMapping("/export-vaccine")
+    public ResponseEntity<Vaccine> exportVaccine(@RequestParam Long id,@RequestParam Integer newAmount) {
+        Vaccine vaccine = vaccineService.findById(id);
+        if (vaccine == null) {
+            return ResponseEntity.notFound().build();
+        }
+        vaccine.setAmount(newAmount);
+        return ResponseEntity.ok(vaccineService.save(vaccine));
+    }
+
+
+
+}
--- src/main/java/com/c0320g1/vaccine/service/Impl/VaccineServiceImpl.java
+++ src/main/java/com/c0320g1/vaccine/service/Impl/VaccineServiceImpl.java
@@ -3,14 +3,43 @@ package com.c0320g1.vaccine.service.Impl;
 import com.c0320g1.vaccine.model.Vaccine;
 import com.c0320g1.vaccine.repository.VaccineRepository;
 import com.c0320g1.vaccine.service.VaccineService;
+import com.c0320g1.vaccine.service.search.VaccineSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class VaccineServiceImpl implements VaccineService {
 
     @Autowired
     private VaccineRepository vaccineRepository;
+
+    @Override
+    public Page<Vaccine> findBySpec(String name, String category, String country, String inventoryStatus, int page) {
+        Sort sort = Sort.by(Sort.Direction.ASC,"expiryDate");
+        Pageable pageable = PageRequest.of(page,2,sort);
+        Specification<Vaccine> specVaccine = null;
+        List<VaccineSpecification> vaccineSpecList = new ArrayList<>();
+        vaccineSpecList.add(new VaccineSpecification("name","like",name));
+        vaccineSpecList.add(new VaccineSpecification("category","like",category));
+        vaccineSpecList.add(new VaccineSpecification("country","like",country));
+        vaccineSpecList.add(new VaccineSpecification("inventoryStatus","like",inventoryStatus));
+        if (!vaccineSpecList.isEmpty()) {
+            specVaccine = Specification.where(vaccineSpecList.get(0));
+            for (int i = 1; i < vaccineSpecList.size(); i++) {
+                assert specVaccine != null;
+                specVaccine = specVaccine.and(vaccineSpecList.get(i));
+            }
+        }
+        return vaccineRepository.findAll(specVaccine,pageable);
+    }
 
     @Override
     public Vaccine save(Vaccine vaccine) {
