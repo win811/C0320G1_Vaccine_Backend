3485d1ae5e624f5a2b4bc1c6493967e05c309c78
NguyenDucThong <Nguyenducthong22>
2020-09-30 08:50:37 +0700

    C-Thong

--- src/main/java/com/c0320g1/vaccine/repository/InjectionHistoryRepository.java
+++ src/main/java/com/c0320g1/vaccine/repository/InjectionHistoryRepository.java
@@ -1,9 +1,14 @@
 package com.c0320g1.vaccine.repository;
 
+import com.c0320g1.vaccine.model.DailySchedule;
 import com.c0320g1.vaccine.model.InjectionHistory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InjectionHistoryRepository extends JpaRepository<InjectionHistory, Long> {
+    //thong
+    Page<InjectionHistory> findAllByVaccine_Id(Long vaccine_id, Pageable pageable);
 }
